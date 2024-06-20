9735bcb4d2548336e2eb1751c42e7649daeb85c8
verozroke <73778760+verozroke@users.noreply.github.com>
2024-06-20 11:49:15 +0500

    done

--- components/Info/Section.vue
+++ components/Info/Section.vue
@@ -18,19 +18,22 @@
         </p>
       </div>
       <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-        <div class="relative flex flex-col items-center pb-20 subtitle hidden1">
-          <div class="flex h-10 w-10 items-center justify-center rounded-lg bg-darkblue ">
-            <img
-              class="h-6 w-6 text-white"
-              src="~/assets/images/icons/global.png"
-              alt=""
-            >
-          </div>
-          <dt class="text-base font-semibold leading-7 text-center text-crush-pink">
-            {{ $t('info.points.1.title') }}
-          </dt>
-        </div>
         <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+          <div class="relative pl-16 title hidden1">
+            <dt class="text-base font-semibold leading-7 text-crush-pink">
+              <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-darkblue">
+                <img
+                  class="h-6 w-6 text-white"
+                  src="~/assets/images/icons/global.png"
+                  alt=""
+                >
+              </div>
+              {{ $t('info.points.1.title') }}
+            </dt>
+            <dd class="mt-2 text-base leading-7 text-white">
+              {{ $t('info.points.1.description') }}
+            </dd>
+          </div>
           <div class="relative pl-16 title hidden1">
             <dt class="text-base font-semibold leading-7 text-crush-pink">
               <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-darkblue">
