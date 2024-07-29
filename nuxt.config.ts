fa67bae239655c25fedd16b3eb369b0a183814a9
verozroke <73778760+verozroke@users.noreply.github.com>
2024-07-29 12:01:58 +0500

    yandex metrics added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,32 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/png', href: '/logo.png' }
       ],
+      meta: [
+        { name: 'google-site-verification', content: '39CKGqrHvhiXFbGzqR-39VSYfBfbzcHJrLqde59Z6xw' },
+        { name: 'yandex-verification', content: '0905eb56b8731059' },
+        { name: 'google-site-verification', content: '39CKGqrHvhiXFbGzqR-39VSYfBfbzcHJrLqde59Z6xw' },
+      ],
+      script: [
+        {
+          type: 'text/javascript',
+          innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                      m[i].l=1*new Date();
+                      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                      ym(97935596, "init", {
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true
+                      });
+                    `
+        },
+      ],
+      noscript: [
+        { innerHTML: `<div><img src="https://mc.yandex.ru/watch/97935596" style="position:absolute; left:-9999px;" alt="" /></div>` }
+      ]
     }
   },
   devtools: { enabled: true },
